e57940ad14acced3c1be2c4fbbffe45c98717a26
Rifat-Hossain-Khan <rifat.hk889@gmail.com>
2021-11-11 23:10:27 +0600

    screenshot code added

--- wdio.conf.js
+++ wdio.conf.js
@@ -137,7 +137,7 @@ exports.config = {
     reporters: [['allure', {
         outputDir: 'allure-results',
         disableWebdriverStepsReporting: true,
-        disableWebdriverScreenshotsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
     }]],
 
 
@@ -234,8 +234,11 @@ exports.config = {
      * @param {Boolean} result.passed    true if test has passed, otherwise false
      * @param {Object}  result.retries   informations to spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
+    afterTest: function(test, context, { error, result, duration, passed, retries }) {
+        if (error) {
+            browser.takeScreenshot();
+          }
+    },
 
 
     /**
